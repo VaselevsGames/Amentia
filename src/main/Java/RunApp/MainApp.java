d963c002fb1835489dff70845fdfb14b3918137d
NstTaInt <ilja.kol4anoff@gmail.com>
2015-03-17 22:05:57 +0500

    Correction MainApp (problem with Cyrillic characters in the title).
    *V rot ebal svoyu udachu - perestala rabota`t russkaya raskladka!

--- src/main/Java/RunApp/MainApp.java
+++ src/main/Java/RunApp/MainApp.java
@@ -1,9 +1,5 @@
 package RunApp;
 
-/**
- * Created by TaInt on 15.03.2015.
- */
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -19,9 +15,9 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         String fxmlFile = "/fxml/game.fxml";
-        FXMLLoader loader = new FXMLLoader();
-        Parent root = loader.load(getClass().getResource(fxmlFile));
-        stage.setTitle("Аменция: История Горла");
+        new FXMLLoader();
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+        stage.setTitle("Amentia: history Throat");
         stage.setScene(new Scene(root));
         stage.setResizable(false);
         stage.show();
